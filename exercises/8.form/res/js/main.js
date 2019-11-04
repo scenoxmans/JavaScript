cfab6a5b1e6ffebd5ffc781adc5c81d960a40b93
scenoxmans <mansurnaurkhanov@mail.com>
2019-11-04 07:26:16 +0100

    fixing stuff

---
+++ exercises/8.form/res/js/main.js
@@ -0,0 +1,27 @@
+let myform = document.getElementById('myform');
+let name = document.getElementById('name');
+let lastname = document.getElementById('lastname');
+let username = document.getElementById('username');
+let email = document.getElementById('email');
+let password = document.getElementById('password');
+let confirmpassword = document.getElementById('confirmpassword');
+let adress = document.getElementById('adress');
+let city = document.getElementById('city');
+let zip = document.getElementById('zip');
+let phone = document.getElementById('phone');
+let age = document.getElementById('age');
+let hobbies = document.getElementById('hobbies');
+let submit = document.getElementById('submit');
+
+
+    //    let dataname     = name.value;
+    //    let datalastname = lastname.value;
+    //    let datausername = username.value;
+    //    let dataemail    = email.value;
+    //    let datapassword = password.value;
+    //    let dataadress   = adress.value;
+    //    let datacity     = city.value;
+    //    let datazip      = zip.value;
+    //    let dataphone    = phone.value;
+    //    let dataage      = age.value;
+    //    let datahobbies  = hobbies.value; 
